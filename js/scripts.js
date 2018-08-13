1363c6072287007795c9964f075fa6fe3a8cbca6
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2018-08-13 17:24:32 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,6 @@ sortAncher.addEventListener('click', () =>{
 
 
 $(document).ready(function(){
-// check wether jquery working or not
-  // $('h3').click(function(){
-  //   $(this).css('background-color', '#ff0000');
-  // })
-  //
-  // $('h2').click(function(){
-  //   $(this).css('background-color','#ccc')
-  // })
 
 $('.js__list--icon').click(function(){
   var nav = $('.header__list--2');
