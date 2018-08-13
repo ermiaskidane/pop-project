f8a5c7696e2e73b151331b23c97cd76006e5f0af
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2018-08-13 17:23:24 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 
 $(document).ready(function(){
-// check wether jquery working or not
-  // $('h3').click(function(){
-  //   $(this).css('background-color', '#ff0000');
-  // })
-  //
-  // $('h2').click(function(){
-  //   $(this).css('background-color','#ccc')
-  // })
 
 $('.js__list--icon').click(function(){
   var nav = $('.header__list--2');
