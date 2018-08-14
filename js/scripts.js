b0d09c21cdea4ce0e38bde0b4b03c55408e31c06
ermias kidane <hadeklte21@gmail.com>
2018-08-14 13:13:59 +0100

    get popup and sortby from index to tv html using js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-const popGenre = document.querySelector('.pop-ancher');
-const
+const popAncher = document.querySelector('.pop-ancher');
+const sortAncher = document.querySelector('.sort-ancher');
 
-popGenre.addEventListener('click', () =>{
+popAncher.addEventListener('click', () =>{
   const popup = document.querySelector('.popup');
-
   popup.classList.toggle('open');
+});
 
+
+sortAncher.addEventListener('click', () =>{
+    const sortBy = document.querySelector('.sortby');
+    sortBy.classList.toggle('open');
 })
+
 
 
 
